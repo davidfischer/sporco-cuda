8e3681bcc58124254e4e77718598e7fcd92f1a04
David Fischer <djfische@gmail.com>
2018-05-08 13:03:07 -0700

    Remove html_style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -155,7 +155,7 @@ html_theme = "haiku"
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
 # given in html_static_path.
-html_style = 'sporco.css'
+#html_style = 'sporco.css'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
